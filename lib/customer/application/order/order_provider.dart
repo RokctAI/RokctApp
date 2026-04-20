e16e37bcf8eda5d3c5dcac986ba9b2b6be53020e
Rendani Sinyage <sinyage@gmail.com>
2026-04-20 12:34:49 +0200

    Update order_provider.dart

--- lib/customer/application/order/order_provider.dart
+++ lib/customer/application/order/order_provider.dart
@@ -3,6 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rokctapp/customer/application/order/order_notifier.dart';
 import 'package:rokctapp/customer/application/order/order_state.dart';
 
-final orderProvider = NotifierProvider.autoDispose<OrderNotifier, OrderState>(
+final orderProvider = NotifierProvider<OrderNotifier, OrderState>(
   () => OrderNotifier(),
 );
