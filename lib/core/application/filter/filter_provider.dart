19bf14087e0d4e7fdccd6c9624281fc6ad22120b
Rendani Sinyage <sinyage@gmail.com>
2026-04-20 12:34:43 +0200

    Update filter_provider.dart

--- lib/core/application/filter/filter_provider.dart
+++ lib/core/application/filter/filter_provider.dart
@@ -4,6 +4,6 @@ import 'package:rokctapp/core/application/filter/filter_notifier.dart';
 import 'package:rokctapp/core/application/filter/filter_state.dart';
 
 final filterProvider =
-    NotifierProvider.autoDispose<FilterNotifier, FilterState>(
+    NotifierProvider<FilterNotifier, FilterState>(
       () => FilterNotifier(),
     );
