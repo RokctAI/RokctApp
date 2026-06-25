cdf8a275e46714dc100595f889743dcb5e0ae11b
Rendani Sinyage <sinyage@gmail.com>
2026-06-25 16:13:21 +0200

    Update productivity_sdk.dart

--- sdk/productivity_sdk/lib/productivity_sdk.dart
+++ sdk/productivity_sdk/lib/productivity_sdk.dart
@@ -2,3 +2,5 @@ library productivity_sdk;
 
 export 'src/task_model.dart';
 export 'src/task_service.dart';
+export 'src/domain/interface/todo_repository_facade.dart';
+export 'src/infrastructure/repositories/todo_repository_impl.dart';
