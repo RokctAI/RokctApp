94c556b5a8d4ac1b50c0562555f1140576f7bd4b
Rendani Sinyage <sinyage@gmail.com>
2026-06-25 18:25:37 +0200

    update

--- sdk/productivity_sdk/lib/src/application/tasks/tasks_provider.dart
+++ sdk/productivity_sdk/lib/src/application/tasks/tasks_provider.dart
@@ -1,46 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:core_sdk/core_sdk.dart';
-import 'task_service.dart';
+import 'package:productivity_sdk/productivity_sdk.dart';
+import '../../infrastructure/services/task_service.dart';
+import 'tasks_notifier.dart';
+import 'tasks_state.dart';
 
 final taskServiceProvider = Provider<TaskService>((ref) {
   final database = ref.watch(appDatabaseProvider);
   return TaskService(database);
 });
 
-final tasksStateProvider = StateNotifierProvider<TasksNotifier, List<TaskModel>>((ref) {
+final tasksStateProvider = StateNotifierProvider<TasksNotifier, TasksState>((ref) {
   final service = ref.watch(taskServiceProvider);
   return TasksNotifier(service);
 });
-
-class TasksNotifier extends StateNotifier<List<TaskModel>> {
-  TasksNotifier(this._service) : super([]) {
-    loadTasks();
-  }
-
-  final TaskService _service;
-
-  Future<void> loadTasks() async {
-    final tasks = await _service.getTasks();
-    state = tasks;
-  }
-
-  Future<void> addTask(TaskModel task) async {
-    await _service.addTask(task);
-    await loadTasks();
-  }
-
-  Future<void> updateTask(TaskModel task) async {
-    await _service.updateTask(task);
-    await loadTasks();
-  }
-
-  Future<void> deleteTask(String id) async {
-    await _service.deleteTask(id);
-    await loadTasks();
-  }
-
-  Future<void> transitionTask(String id, ProcessingState newState) async {
-    await _service.transitionTask(id, newState);
-    await loadTasks();
-  }
-}
